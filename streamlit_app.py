43b5a1e9ff8dab74b35d008498e7b2e964fb5bee
yvettehu797 <yvettehu797@gmail.com>
2025-05-21 02:07:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,6 +99,7 @@ class ChatBot:
             messages=self.messages,
             prompt=message,
             stream=True,
+            flow_stream_mode="agent_format"),
             incremental_output=True
         )
         
